98849f9a67192cf80437b1b2dfc200128234e428
Jim Sutton <jimsuttonjimsutton@gmail.com>
2014-09-27 16:25:50 -0600

    Add Posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+
+  get 'posts/index'
+
+  get 'posts/new'
+
+  get 'posts/edit'
+
+  get 'posts/show'
+
+  root 'posts#index'
+  resources :posts
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
